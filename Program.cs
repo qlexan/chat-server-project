be2d1b53c7a6d0602180e81322026c658c557971
Alexander Friberg <alexanderfriberg1999@gmail.com>
2025-09-15 02:14:39 +0200

    fix: switched server connection to happen after client connection

--- Program.cs
+++ Program.cs
@@ -9,15 +9,14 @@ ISocketHandler serverHandler = server;
 
 server.BindAndListen(endPoint);
 
-System.Console.WriteLine("[SERVER] Waiting for connection...");
-Socket clientAccepted = server.Accept();
-System.Console.WriteLine("[SERVER] Client connected.");
-
 Client client = new();
 ISocketHandler clientHandler = client;
 client.Connect(endPoint);
 System.Console.WriteLine("[CLIENT] Connected to server.");
 
+System.Console.WriteLine("[SERVER] Waiting for connection...");
+Socket clientAccepted = server.Accept();
+System.Console.WriteLine("[SERVER] Client connected.");
 
 clientHandler.SendMessage("Hello from client");
 string serverReceived = serverHandler.ReceiveMessage(socket:clientAccepted);
