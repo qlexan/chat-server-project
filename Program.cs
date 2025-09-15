933bd7d861b3374a307b9d37c9c516d920747feb
Alexander Friberg <alexanderfriberg1999@gmail.com>
2025-09-15 02:08:25 +0200

    feat: added main function logic for testing client and server

--- Program.cs
+++ Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using SocketLib;
+using System.Net.Sockets;
+using System.Net;
+
+var endPoint = new IPEndPoint(IPAddress.Loopback, 12345);
+
+Server server = new();
+ISocketHandler serverHandler = server;
+
+server.BindAndListen(endPoint);
+
+System.Console.WriteLine("[SERVER] Waiting for connection...");
+Socket clientAccepted = server.Accept();
+System.Console.WriteLine("[SERVER] Client connected.");
+
+Client client = new();
+ISocketHandler clientHandler = client;
+client.Connect(endPoint);
+System.Console.WriteLine("[CLIENT] Connected to server.");
+
+
+clientHandler.SendMessage("Hello from client");
+string serverReceived = serverHandler.ReceiveMessage(socket:clientAccepted);
+System.Console.WriteLine("[SERVER] Received: "+ serverReceived);
+
+serverHandler.SendMessage("Hello Client", clientAccepted);
+string clientReceived = clientHandler.ReceiveMessage();
+System.Console.WriteLine("[CLIENT] Received: " + clientReceived);
